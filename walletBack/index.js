d451fb4c2a831ebc821e152354613df74b6e0e64
JerryGarcia07 <jerry_garcia_07@hotmail.com>
2025-09-01 23:01:39 -0500

    crud

--- walletBack/index.js
+++ walletBack/index.js
@@ -1,69 +1,71 @@
 //import categoria from './data.js';
 import express from 'express';
+import {PORT} from "./config.js"
+import useRouter from './router/taks.routers.js';
 
 const app= express();
-const PORT=3000;
+
 
 //Middlewaree para leer json
-app.use(express.json())
+app.use(useRouter)
 
 
-let categoria = [
-  { id: 1, titulo: "Comida"},
-  { id: 2, titulo: "Pasaje" },
-  { id: 3, titulo: "Paga" },
-];
+// let categoria = [
+//   { id: 1, titulo: "Comida"},
+//   { id: 2, titulo: "Pasaje" },
+//   { id: 3, titulo: "Paga" },
+// ];
 
-/* ========== RUTAS DEL CRUD ========== */
+// /* ========== RUTAS DEL CRUD ========== */
 
-// READ - Obtener todos los libros
-app.get('/categoria', (req, res)=>{
-   res.json(categoria)
-})
+// // READ - Obtener todos los libros
+// app.get('/categoria', (req, res)=>{
+//    res.json(categoria)
+// })
 
-// READ - Obtener un libro por su ID
-app.get('/categoria/:id', (req, res)=>{
-    const id=parseInt(req.params.id)
-    const cate= categoria.find(c=>c.id===id);
-    if(!cate){
-        return res.status(404).json({mensaje:"Categoria no encontrado"})
-    }
-    res.json(cate)
-})
+// // READ - Obtener un libro por su ID
+// app.get('/categoria/:id', (req, res)=>{
+//     const id=parseInt(req.params.id)
+//     const cate= categoria.find(c=>c.id===id);
+//     if(!cate){
+//         return res.status(404).json({mensaje:"Categoria no encontrado"})
+//     }
+//     res.json(cate)
+// })
 
 
-// CREATE - Agregar un nuevo libro
-app.post('/categoria', (req, res)=>{
-    const {titulo}=req.body
-    const nuevoCategoria={
-        id: categoria.length + 1,
-        titulo
-    }
+// // CREATE - Agregar un nuevo libro
+// app.post('/categoria', (req, res)=>{
+//     const {titulo}=req.body
+//     const nuevoCategoria={
+//         id: categoria.length + 1,
+//         titulo
+//     }
 
-    categoria.push(nuevoCategoria)
-    res.status(201).json(nuevoCategoria)
-})
+//     categoria.push(nuevoCategoria)
+//     res.status(201).json(nuevoCategoria)
+// })
 
-// UPDATE - Modificar un libro existente
-app.put('/categoria/:id', (req, res)=>{
-    const id=parseInt(req.params.id)
-    const {titulo}=req.body
+// // UPDATE - Modificar un libro existente
+// app.put('/categoria/:id', (req, res)=>{
+//     const id=parseInt(req.params.id)
+//     const {titulo}=req.body
 
-    const cate= categoria.find(c=>c.id===id);
-    if(!cate){
-        return res.status(404).json({mensaje:"Categoria no encontrado"})
-    }
+//     const cate= categoria.find(c=>c.id===id);
+//     if(!cate){
+//         return res.status(404).json({mensaje:"Categoria no encontrado"})
+//     }
 
-    cate.titulo=titulo
-    res.json(categoria)
-})
+//     cate.titulo=titulo
+//     res.json(categoria)
+// })
 
-// DELETE - Eliminar un libro
-app.delete('/categoria/:id', (req, res)=>{
-    const id=parseInt(req.params.id)
-    categoria = categoria.filter(c=>c.id!==id);
-    res.json({mensaje: "categoria eliminado"})
-})
+// // DELETE - Eliminar un libro
+// app.delete('/categoria/:id', (req, res)=>{
+//     const id=parseInt(req.params.id)
+//     categoria = categoria.filter(c=>c.id!==id);
+//     res.json({mensaje: "categoria eliminado"})
+// })
 
 //iniciar el servidor
 app.listen(PORT, ()=>{
---
+++ walletBack/router/taks.routers.js
@@ -0,0 +1,32 @@
+import { Router } from "express";
+const router=Router();
+
+//obtener una categoria
+router.get("/categorias", (req, res)=>{
+    res.send("obtenido Usuario")
+})
+//obtener una categoria por id
+router.get("/categorias/:id", (req, res)=>{
+    const {id}=req.params
+    res.send(`obtenido Usuario ${userId}`)
+})
+
+//agrego una categoria
+router.post("/categorias", (req, res)=>{
+    res.send("Creando Categoria")
+})
+
+//actualizar categoria
+router.put("/categorias/:id", (req, res)=>{
+    const {id}=req.params
+    res.send(`actualizando actegoria ${id}`)                 
+})
+
+//eliminar categoria
+router.delete("/categorias/:id", (req, res)=>{
+    const {id}=req.params
+    res.send(`elimina categoria ${id}`)
+})
+
+
+export default router
