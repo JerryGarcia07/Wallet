408d258d11916cdbc9eb73751473312409946c73
JerryGarcia07 <jerry_garcia_07@hotmail.com>
2025-09-03 23:56:37 -0500

    conectar base de datos

--- walletBack/db.js
+++ walletBack/db.js
@@ -1,1 +1,10 @@
 //db.js
+import pg from "pg";
+
+export default pool =new pg.Pool({
+    user:"postgres",
+    host: "localhost",
+    password: "147536982",
+    database: "walletbd",
+    port: "5432"
+})
