17030a4c50a1e6ca65409948e11f28a4d5b90103
JerryGarcia07 <jerry_garcia_07@hotmail.com>
2025-09-10 23:56:47 -0500

    crud uno ccasi completo

---
+++ walletBack/controllers/categoria.controllers.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { getCategoria, getCategorias, postCategoria, putCategoria, deleteCategoria } from "../router/categoria.routers";
+
+
+const router=Router();
+
+router.get("/categorias", getCategorias)
+router.get("/categorias/:id", getCategoria)
+router.post("/categorias", postCategoria)
+router.put("/categorias/:id", putCategoria)
+router.delete("/categorias/:id",deleteCategoria)
+
+export default router
