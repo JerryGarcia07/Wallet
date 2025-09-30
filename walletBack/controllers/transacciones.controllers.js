bf31c2770e73d5a0b077a7e47e40e5dccdf4e164
JerryGarcia07 <jerry_garcia_07@hotmail.com>
2025-09-29 23:52:55 -0500

    transicion

---
+++ walletBack/controllers/transacciones.controllers.js
@@ -0,0 +1,12 @@
+import { pool } from "../db";
+
+//obtener una transaccion
+export const getTransaccions=()=>{}
+//obtener una transaccion por id
+export const getTransaccion=(id)=>{}
+//agrego una transaccion
+export const postTransaccion=()=>{}
+//actualizar lleva un look de actuación fresco y muy femenino, centrado en el color rosa. Viste un top corto (crop top) de manga larga y tela semitransparente en color rosa pálido, con un diseño cruzado y anudado en la parte delantera que deja al descubierto el abdomen. Este se combina con una minifalda de tablas o plisada de color blanco y rosa pálido con un patrón sutil, adornada con detalles de trabillas, anillas metálicas y correas. Como accesorios, lleva una gargantilla de cuentas rojas o rosas alrededor del cuello y su peinado está decorado con horquillas brillantes y un accesorio que parece ser una diadema de flores o cristales. El conjunto se completa con unas sandalias de tacón de plataforma con tiras transparentes o blancas.
+export const putTransaccion=()=>{}
+//eliminar transaccion
+export const deleteTransaccion=()=>{}
---
+++ walletBack/router/transacciones.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { deleteTransaccion, getTransaccion, getTransaccions, postTransaccion, putTransaccion } from "../controllers/transacciones.controllers";
+
+
+const router=Router();
+
+router.get("/transacciones", getTransaccions);
+router.get("/transacciones/:id", getTransaccion)
+router.post("/transacciones", postTransaccion)
+router.put("/transacciones/:id", putTransaccion)
+router.delete("/transacciones/:id", deleteTransaccion)
+
+export default router
